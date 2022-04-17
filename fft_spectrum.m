3efb7abcee3620d9713afb4ef21cccd1c84dac11
ageva2022 <ageva@umass.edu>
2022-04-17 18:09:51 -0400

    Adding SVD images and FT spectrums

--- fft_spectrum.m
+++ fft_spectrum.m
@@ -1,4 +1,5 @@
-I = imread('fingerprintARCH_svd.jpg');
+%Change input image accoirdingly
+I = imread('fingerprintWHORL_svd.jpg');
 
 F = fft2(I);
 
@@ -7,8 +8,7 @@ F = fftshift(F); % Center FFT
 F = abs(F); % Get the magnitude
 F = log(F+1); % Use log, for perceptual scaling, and +1 since log(0) is undefined
 
-%F = mat2gray(F); % Use mat2gray to scale the image between 0 and 1
-
 imshow(F,[]); % Display the result
 
+%TO DO: save the image in the script rather than doing it manually
 %imwrite((F,[]),'spectrum_arch.jpg')

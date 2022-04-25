78f902721f003d5f450b1107e9fad0a28d403032
Dhruvi Chauhan <dhruvinc05@gmail.com>
2022-04-25 09:43:43 -0400

    Updated the img comparison file

--- img_comparison.m
+++ img_comparison.m
@@ -1,21 +1,27 @@
 clc 
 clear
 
-pic1 = uint8('fingerprintARCH_ft.jpg');
-pic2 = uint8('fingerprintLOOP_ft.jpg');
-whorl = uint8('fingerprintWHORL_ft.jpg');
-%pic4 = uint8('WHORL_FFT.jpg');
-%pic5 = uint8('LOOP_FFT.jpg');
-%pic6 = uint8('ARCH_FFT.jpg');
-imsubtract(pic1,pic2)
+arch = 'fingerprintARCH_ft.jpg';
+loop = 'fingerprintLOOP_ft.jpg';
+whorl = 'fingerprintWHORL_ft.jpg';
 
-[x1,y1] = size(pic1);
-[x2,y2] = size(pic2);
+img_comp(arch,whorl);
 
-if(x1~=x2 || y1~=y2)
-    'Images must be of the same dimension';
-else 
-    k = pic1==pic2 ;   % get the equal elements 
-    iwant = sum(k(:));   % total number of equal elements
-    percentage = iwant/(x1*y1)*100
+function [percent] = img_comp(orig,comp)
+
+    pic1 = uint8(orig);
+    pic2 = uint8(comp);
+    
+    [x1,y1] = size(pic1);
+    [x2,y2] = size(pic2);
+    
+    if(x1~=x2 || y1~=y2)
+        disp('Images must have the same size and class.');
+
+    else 
+        imsubtract(pic1,pic2)
+        k = pic1==pic2 ;   % get the equal elements 
+        iwant = sum(k(:));   % total number of equal elements
+        percent = iwant/(x1*y1)*100
+    end
 end
